9aebc69ee0aa5fccfd8579070a24287a0e5d19ba
carterross2 <carterross2@me.com>
2025-09-12 03:30:08 -0600

    removed old stuff

--- extensions/elastic_backup_20250911_020045/providers/aws/linux.tf
+++
@@ -1,9 +0,0 @@
-"elastic" = {
-  name               = "elastic"
-  linux_sku          = "22_04-lts-gen2"
-  linux_version      = "latest"
-  ami                = "ami-00c71bd4d220aa22a"
-  private_ip_address = "{{ip_range}}.52"
-  password           = "sgdvnkjhdshlsd"
-  size               = "t3.xlarge"  # 4cpu / 16GB for Elastic Stack
-}
